c42a4d17ba930d8717bc6ea89d29e1507eb0a919
vermosen <vermosen.jm@free.fr>
2016-07-31 11:47:48 -0400

    added missing linker reference

--- src/database.cpp
+++ src/database.cpp
@@ -1,18 +1,31 @@
 /*
  * database.cpp
  *
- *  Created on: Jul 31, 2016
+ *  Created on: Jul 3, 2016
  *      Author: vermosen
  */
 
 #include "database.hpp"
 
-database::database() {
-	// TODO Auto-generated constructor stub
+namespace db
+{
+	connector::connector(boost::shared_ptr<logger> log) : log_(log) {}
 
+	connector::~connector() {}
+
+	boost::shared_ptr<soci::session> connector::session()
+	{
+		return session_;
+	}
+
+	database::database(boost::shared_ptr<logger> & log) : log_(log) {}
+
+	database::~database() {}
+
+	void database::connect(const std::string & connectionString)
+	{
+		connector_->connect(connectionString);
+	}
+
+	boost::shared_ptr<connector> database::dbConnector() { return connector_; }
 }
-
-database::~database() {
-	// TODO Auto-generated destructor stub
-}
-
--- src/database.hpp
+++ src/database.hpp
@@ -8,10 +8,47 @@
 #ifndef DATABASE_HPP_
 #define DATABASE_HPP_
 
-class database {
-public:
-	database();
-	virtual ~database();
-};
+#include <boost/shared_ptr.hpp>
+
+#include <soci.h>
+
+#include "logger.hpp"
+
+namespace db
+{
+	class connector
+	{
+	public:
+		connector(boost::shared_ptr<logger> log);
+		virtual ~connector();
+		virtual void connect(const std::string & connectionString) = 0;
+		boost::shared_ptr<soci::session> session();
+
+		void begin		() {session_->begin		(); }
+		void commit		() {session_->commit	(); }
+		void rollback	() {session_->rollback	(); }
+
+	protected:
+		boost::shared_ptr<soci::session> session_;
+		boost::shared_ptr<logger> log_;
+	};
+
+	class database
+	{
+	public:
+		database(boost::shared_ptr<logger> &);
+		virtual ~database();
+
+		void connect(const std::string & connectionString);
+		boost::shared_ptr<connector> dbConnector();
+
+		virtual bool checkStatus() const = 0;
+		virtual bool rebuild() = 0;
+
+	protected:
+		boost::shared_ptr<connector> connector_;
+		boost::shared_ptr<logger> log_;
+	};
+}
 
 #endif /* DATABASE_HPP_ */
